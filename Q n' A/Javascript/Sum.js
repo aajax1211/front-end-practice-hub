9e877a9cd4dd98dca15ba2850f5cc1d6a67b5b3a
Ajit Behl <Abehl3036@conestogac.on.ca>
2025-01-11 21:30:04 -0500

    Add sum function for chained addition with final result return

---
+++ Q n' A/Javascript/Sum.js
@@ -0,0 +1,26 @@
+// Implement a sum function that accepts a number 
+// and allows for repeated calling with more numbers.
+// Calling the function without an argument will sum up
+// all the arguments thus far and return the total.
+
+// Examples
+
+// sum(1)(); // 1
+// sum(1)(2)(); // 3
+// sum(1)(2)(-3)(); // 0
+
+function sum(value) {
+    return function (nextValue) {
+        if(nextValue === undefined){
+            return value
+      }else{
+        return sum(value + nextValue);
+      }
+    
+    }
+  }
+
+
+
+
+console.log(sum(2)(3)(-2)())
